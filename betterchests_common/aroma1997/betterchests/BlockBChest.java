67e06ed7d7ea9e01c683d2177099f801e71250b6
Aroma1997 <robin.marius@web.de>
2013-08-03 13:38:56 +0200

    Made Chests not connect together.

--- betterchests_common/aroma1997/betterchests/BlockBChest.java
+++ betterchests_common/aroma1997/betterchests/BlockBChest.java
@@ -2,6 +2,7 @@ package aroma1997.betterchests;
 
 import net.minecraft.block.BlockChest;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.MathHelper;
@@ -23,7 +24,7 @@ public class BlockBChest extends BlockChest {
 		return new TileEntityBChest();
 	}
 	
-	/*@Override
+	@Override
     public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
     {
 		if (par5EntityPlayer.getHeldItem() == null || !(par5EntityPlayer.getHeldItem().getItem() instanceof ItemUpgrade)) return super.onBlockActivated(par1World, par2, par3, par4, par5EntityPlayer, par6, par7, par8, par9);
@@ -32,7 +33,7 @@ public class BlockBChest extends BlockChest {
 		
 		return te.upgrade(par5EntityPlayer, par1World);
 		
-    }*/
+    }
 	
 	@Override
     public int isProvidingWeakPower(IBlockAccess par1IBlockAccess, int par2, int par3, int par4, int par5)
@@ -49,24 +50,39 @@ public class BlockBChest extends BlockChest {
     }
 	
 	@Override
-    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+    public void unifyAdjacentChests(World par1World, int par2, int par3, int par4)
     {
-		try {
-			TileEntityChest te = (TileEntityChest) par1World.getBlockTileEntity(par2, par3, par4);
-			te.adjacentChestXNeg = te.adjacentChestXPos = te.adjacentChestZNeg = te.adjacentChestZPosition = null;
-		}
-		catch(Exception e) {}
-		if (par5EntityPlayer.getHeldItem() == null || !(par5EntityPlayer.getHeldItem().getItem() instanceof ItemUpgrade)) return super.onBlockActivated(par1World, par2, par3, par4, par5EntityPlayer, par6, par7, par8, par9);
+    	
+    }
+	
+	@Override
+    public void onBlockAdded(World par1World, int par2, int par3, int par4)
+    {
 		
-		TileEntityBChest te = (TileEntityBChest) par1World.getBlockTileEntity(par2, par3, par4);
-		
-		return te.upgrade(par5EntityPlayer, par1World);
     }
 	
 	@Override
     public int getLightValue(IBlockAccess world, int x, int y, int z)
     {
 		return ((TileEntityBChest)world.getBlockTileEntity(x, y, z)).getLightValue();
+    }
+	
+	@Override
+    public IInventory getInventory(World par1World, int par2, int par3, int par4)
+    {
+        Object object = (TileEntityChest)par1World.getBlockTileEntity(par2, par3, par4);
+
+        if (object == null)
+        {
+            return null;
+        }
+        return (IInventory)object;
+    }
+	
+	@Override
+    public void setBlockBoundsBasedOnState(IBlockAccess par1IBlockAccess, int par2, int par3, int par4)
+    {
+		
     }
 	
 }
