8c09bc9549b6b2ac6361430fd2c317a6d66efc50
Aroma1997 <robin.marius@web.de>
2013-10-21 15:16:39 +0200

    Fixed Chest facing the wron direction. Fixes #5

--- betterchests_common/aroma1997/betterchests/BlockBChest.java
+++ betterchests_common/aroma1997/betterchests/BlockBChest.java
@@ -17,11 +17,13 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -247,5 +249,33 @@ public class BlockBChest extends BlockContainer {
 	{
 		return true;
 	}
+	
+	@Override
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+    {
+        byte facing = 0;
+        int rotation = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (rotation == 0)
+        {
+            facing = 2;
+        }
+
+        if (rotation == 1)
+        {
+            facing = 5;
+        }
+
+        if (rotation == 2)
+        {
+            facing = 3;
+        }
+
+        if (rotation == 3)
+        {
+            facing = 4;
+        }
+        par1World.setBlockMetadataWithNotify(par2, par3, par4, facing, 3);
+    }
 	
 }
