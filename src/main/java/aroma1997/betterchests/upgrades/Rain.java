9e4f5cb5d79aa7d59360f568f8bc1738d30ea328
Aroma1997 <robin.marius@web.de>
2014-02-25 21:11:12 +0100

    Made Rain Upgrade work again.

--- src/main/java/aroma1997/betterchests/upgrades/Rain.java
+++ src/main/java/aroma1997/betterchests/upgrades/Rain.java
@@ -3,7 +3,9 @@ package aroma1997.betterchests.upgrades;
 
 
 import aroma1997.betterchests.api.IBetterChest;
+import aroma1997.core.util.InvUtil;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -11,8 +13,15 @@ public class Rain extends BasicUpgrade {
 	
 	@Override
 	public void updateChest(IBetterChest chest, int tick, World world, ItemStack item) {
-		// TODO Auto-generated method stub
-		
+		if (tick != 26) return;
+		ItemStack bucket = InvUtil.getFirstItem(chest, new ItemStack(Items.bucket), true);
+		if (bucket != null) {
+			bucket = bucket.copy();
+			bucket.stackSize = 1;
+			if (InvUtil.putIntoFirstSlot(chest, new ItemStack(Items.water_bucket), false) == null) {
+				InvUtil.getFirstItem(chest, bucket, false);
+			}
+		}
 	}
 	
 }
