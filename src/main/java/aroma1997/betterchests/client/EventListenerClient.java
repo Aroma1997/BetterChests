40f5ea1404677f33d955221e8d8bff5ed3576eef
Aroma1997 <robin.marius@web.de>
2014-02-25 16:31:56 +0100

    Changed KeyBinding.

--- src/main/java/aroma1997/betterchests/client/EventListenerClient.java
+++ src/main/java/aroma1997/betterchests/client/EventListenerClient.java
@@ -4,14 +4,22 @@ package aroma1997.betterchests.client;
 
 import java.lang.reflect.Field;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import org.apache.logging.log4j.Level;
 
 import aroma1997.betterchests.BetterChests;
+import aroma1997.betterchests.ItemBag;
+import aroma1997.betterchests.PacketOpenBag;
 import aroma1997.betterchests.Reference;
+import aroma1997.core.network.NetworkHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -19,6 +27,7 @@ public class EventListenerClient {
 	
 	public EventListenerClient() {
 		MinecraftForge.EVENT_BUS.register(this);
+		FMLCommonHandler.instance().bus().register(this);
 	}
 	
 	private static final String PREFIX = Reference.MOD_ID + ":";
@@ -49,6 +58,20 @@ public class EventListenerClient {
 				}
 				catch (Exception e) {
 					BetterChests.logger.log(Level.ERROR, "Failed to load sound: " + str, e);
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void tick(ClientTickEvent event) {
+		if (BetterChestsKeyBinding.getInstance().getIsKeyPressed()) {
+			NBTTagCompound nbt = new NBTTagCompound();
+			EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+			for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+				if (player.inventory.getStackInSlot(i) != null && player.inventory.getStackInSlot(i).getItem() instanceof ItemBag) {
+					NetworkHelper.sendPacketToPlayers(new PacketOpenBag().setSlot(i));
+					break;
 				}
 			}
 		}
