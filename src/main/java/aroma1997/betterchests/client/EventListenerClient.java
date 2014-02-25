8b36859146d0f26503676d948456006489511635
Aroma1997 <robin.marius@web.de>
2014-02-25 19:50:58 +0100

    Moved sound stuff (still not working)

--- src/main/java/aroma1997/betterchests/client/EventListenerClient.java
+++ src/main/java/aroma1997/betterchests/client/EventListenerClient.java
@@ -20,47 +20,12 @@ import aroma1997.core.network.NetworkHelper;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public class EventListenerClient {
 	
 	public EventListenerClient() {
 		MinecraftForge.EVENT_BUS.register(this);
 		FMLCommonHandler.instance().bus().register(this);
-	}
-	
-	private static final String PREFIX = Reference.MOD_ID + ":";
-	
-	public static final String SOUND_OPEN_CHEST = PREFIX + "bchestopen";
-	
-	public static final String SOUND_CLOSE_CHEST = PREFIX + "bchestclose";
-	
-	@SideOnly(Side.CLIENT)
-	@SubscribeEvent
-	public void loadSounds(SoundLoadEvent event) {
-		for (Field field : getClass().getFields()) {
-			if (! field.isAccessible()) {
-				field.setAccessible(true);
-			}
-			Object o;
-			try {
-				o = field.get(null);
-			}
-			catch (Exception e1) {
-				continue;
-			}
-			if (o != null && o instanceof String && field.getName().startsWith("SOUND_")) {
-				String str = (String) o;
-				BetterChests.logger.log(Level.TRACE, "Loading sound: " + str);
-				try {
-//					event.manager.addSound(str + ".ogg");
-				}
-				catch (Exception e) {
-					BetterChests.logger.log(Level.ERROR, "Failed to load sound: " + str, e);
-				}
-			}
-		}
 	}
 	
 	@SubscribeEvent
