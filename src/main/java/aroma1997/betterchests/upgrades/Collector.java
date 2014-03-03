6ac701bfb93847397b6c58c176ad8349ca466d1a
Aroma1997 <robin.marius@web.de>
2014-03-03 19:25:01 +0100

    Made Collector less annoying for automatically picking up stuff.

--- src/main/java/aroma1997/betterchests/upgrades/Collector.java
+++ src/main/java/aroma1997/betterchests/upgrades/Collector.java
@@ -31,17 +31,9 @@ public class Collector extends BasicUpgrade {
 				}
 				if (e.age >= 10 && e.isEntityAlive())
 				{
-					ItemStack itemBack = InvUtil.putIntoFirstSlot(inv, e.getEntityItem(), false);
-					e.motionX = inv.getXPos() - e.posX;
-					e.motionY = inv.getYPos() - e.posY;
-					e.motionZ = inv.getZPos() - e.posZ;
-					if (itemBack == null) {
-						world.removeEntity(e);
-						// e.setDead();
-					}
-					else {
-						e.setEntityItemStack(itemBack);
-					}
+					e.motionX = (inv.getXPos() - e.posX) / 2;
+					e.motionY = (inv.getYPos() - e.posY) / 2;
+					e.motionZ = (inv.getZPos() - e.posZ) / 2;
 					if (world.rand.nextInt(10) == 0)
 					{
 						float pitch = 0.85F - world.rand.nextFloat() * 3.0F / 10.0F;
@@ -50,6 +42,28 @@ public class Collector extends BasicUpgrade {
 					}
 				}
 			}
+			
+			AxisAlignedBB boundsNew = AxisAlignedBB.getBoundingBox(inv.getXPos() + - 0.5D,
+					inv.getYPos() + 0.0D, inv.getZPos() + - 0.5D, inv.getXPos() + 0.5D,
+					inv.getYPos() + 0.0D, inv.getZPos() + 0.5D);
+				boundsNew = boundsNew.expand(0.2D, 0.2D, 0.2D);
+			List<EntityItem> listNew = world.getEntitiesWithinAABB(EntityItem.class, bounds);
+			
+			for (EntityItem e : listNew) {
+				if (e.age > 10 && e.isEntityAlive()) {
+					if (e.getEntityItem() == null) {
+						continue;
+					}
+					ItemStack itemBack = InvUtil.putIntoFirstSlot(inv, e.getEntityItem(), false);
+					if (itemBack == null) {
+						world.removeEntity(e);
+					}
+					else {
+						e.setEntityItemStack(itemBack);
+					}
+				}
+			}
+			
 		}
 	}
 	
