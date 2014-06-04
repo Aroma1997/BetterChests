11eb9ed9215ec9f14011db0a93d3219cae72e6d1
Aroma1997 <robin.marius@web.de>
2014-06-04 19:20:10 +0200

    Made ticking Upgrade work again.

--- src/main/java/aroma1997/betterchests/upgrades/Ticking.java
+++ src/main/java/aroma1997/betterchests/upgrades/Ticking.java
@@ -1,7 +1,7 @@
 package aroma1997.betterchests.upgrades;
 
 import aroma1997.betterchests.api.IBetterChest;
-
+import aroma1997.core.log.LogHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -9,7 +9,15 @@ public class Ticking extends BasicUpgrade {
 	
 	@Override
 	public void updateChest(IBetterChest chest, int tick, World world, ItemStack item) {
-		// TODO Auto-generated method stub
+		try {
+			for (int i = 0; i < chest.getSizeInventory(); i++) {
+				ItemStack t = chest.getStackInSlot(i);
+				t.updateAnimation(world, null, i, true);
+			}
+		}
+		catch (NullPointerException e) {
+			LogHelper.logException("Failed to use ticking upgrade on chest: " + chest.toString(), e);
+		}
 		
 	}
 	
