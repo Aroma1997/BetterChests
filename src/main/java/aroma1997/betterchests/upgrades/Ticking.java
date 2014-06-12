e08d5f314bf1510185644e7c988600c87ca247ca
Aroma1997 <robin.marius@web.de>
2014-06-12 21:14:18 +0200

    Added information about Ticking Upgrade.

--- src/main/java/aroma1997/betterchests/upgrades/Ticking.java
+++ src/main/java/aroma1997/betterchests/upgrades/Ticking.java
@@ -1,5 +1,8 @@
 package aroma1997.betterchests.upgrades;
 
+import org.apache.logging.log4j.Level;
+
+import aroma1997.betterchests.BetterChests;
 import aroma1997.betterchests.api.IBetterChest;
 import aroma1997.core.log.LogHelper;
 import net.minecraft.item.ItemStack;
@@ -9,6 +12,9 @@ public class Ticking extends BasicUpgrade {
 	
 	@Override
 	public void updateChest(IBetterChest chest, int tick, World world, ItemStack item) {
+		if (tick != 56) {
+			return;
+		}
 		try {
 			for (int i = 0; i < chest.getSizeInventory(); i++) {
 				ItemStack t = chest.getStackInSlot(i);
@@ -16,6 +22,8 @@ public class Ticking extends BasicUpgrade {
 			}
 		}
 		catch (NullPointerException e) {
+			BetterChests.logger.log(Level.WARN, "BetterChests was unable to tick one of the contents in one of the chests.");
+			BetterChests.logger.log(Level.WARN, "Disabling the Upgrade and continuing. This is not a bug, just an information.");
 			LogHelper.logException("Failed to use ticking upgrade on chest: " + chest.toString() + " Coords: x=" + chest.getXCoord() + " y=" + chest.getYCoord() + " z=" + chest.getZCoord(), e);
 			chest.setUpgradeDisabled(item, true);
 		}
