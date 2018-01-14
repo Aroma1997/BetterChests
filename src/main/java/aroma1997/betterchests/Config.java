c60a5e91bd5ec8d731deabdd9cf9231e2b20a52e
Aroma1997 <aroma1997@aroma1997.org>
2018-01-14 12:44:38 +0100

    Fix #100 Config values not changing.

--- src/main/java/aroma1997/betterchests/Config.java
+++ src/main/java/aroma1997/betterchests/Config.java
@@ -18,53 +18,53 @@ public enum Config {
 
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyCobblestone = 0;
+	public int energyCobblestone = 0;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyVoid = 0;
+	public int energyVoid = 0;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyWater = 0;
+	public int energyWater = 0;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energySmelting = 2000;
+	public int energySmelting = 2000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyTicking = 1000;
+	public int energyTicking = 1000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyCollector = 100;
+	public int energyCollector = 100;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyPlanting = 1000;
+	public int energyPlanting = 1000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyHarvesting = 500;
+	public int energyHarvesting = 500;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyBreeding = 1000;
+	public int energyBreeding = 1000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyFeeding = 10000;
+	public int energyFeeding = 10000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyKilling = 1000;
+	public int energyKilling = 1000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyAnimal = 1000;
+	public int energyAnimal = 1000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyPlacer = 1000;
+	public int energyPlacer = 1000;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY)
-	public final int energyBreaker = 1000;
+	public int energyBreaker = 1000;
 
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY_PRODUCTION)
-	public final int energySolar = 10;
+	public int energySolar = 10;
 	@ConfigLimitInt(min = 0)
 	@ConfigEntry(category = CATEGORY_ENERGY_PRODUCTION, description = "This value acts as a multiplier to the vanilla item burn time.")
-	public final int energyFueled = 20;
+	public int energyFueled = 20;
 
 
 	public void load() {
